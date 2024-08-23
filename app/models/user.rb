2d24c333c626a8a4029bf1e761d7ebfcb85fbe84
Johnneil Mendoza <johnneilmendoza@gmail.com>
2024-08-24 00:27:27 +0800

    Add name and phone number fields to donor registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
 
   after_initialize :set_default_role, if: :new_record?
 
+  validates :name, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10}\z/, message: "must be 10 digits long" }
+
   def set_default_role
     self.role ||= :donor
   end
