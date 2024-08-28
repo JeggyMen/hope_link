7952446ddad3c729fe0e630b7b81a346042af952
Johnneil Mendoza <johnneilmendoza@gmail.com>
2024-08-28 17:21:07 +0800

    added the letter opener and the user can receive an email confirmation care of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
-    root 'home#index'
+  root 'home#index'
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
